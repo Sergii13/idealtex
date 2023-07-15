847343e45ea28bf099a673a9f5eaa967479775b4
sergii13 <sergii.khilchuk13@gmail.com>
2023-07-15 15:00:20 +0300

    add new page

--- src/js/files/sliders.js
+++ src/js/files/sliders.js
@@ -55,83 +55,79 @@ function initSliders() {
       },
     });
   }
-  // if (document.querySelector(".swiper")) {
-  //   // Вказуємо склас потрібного слайдера
-  //   // Створюємо слайдер
-  //   new Swiper(".swiper", {
-  //     // Вказуємо склас потрібного слайдера
-  //     // Підключаємо модулі слайдера
-  //     // для конкретного випадку
-  //     modules: [Navigation],
-  //     observer: true,
-  //     observeParents: true,
-  //     slidesPerView: 1,
-  //     spaceBetween: 0,
-  //     autoHeight: true,
-  //     speed: 800,
-  //
-  //     //touchRatio: 0,
-  //     //simulateTouch: false,
-  //     //loop: true,
-  //     //preloadImages: false,
-  //     //lazy: true,
-  //
-  //     /*
-  //                                                                                                           // Ефекти
-  //                                                                                                           effect: 'fade',
-  //                                                                                                           autoplay: {
-  //                                                                                                               delay: 3000,
-  //                                                                                                               disableOnInteraction: false,
-  //                                                                                                           },
-  //                                                                                                           */
-  //
-  //     // Пагінація
-  //     /*
-  //                                                                                                           pagination: {
-  //                                                                                                               el: '.swiper-pagination',
-  //                                                                                                               clickable: true,
-  //                                                                                                           },
-  //                                                                                                           */
-  //
-  //     // Скроллбар
-  //     /*
-  //                                                                                                           scrollbar: {
-  //                                                                                                               el: '.swiper-scrollbar',
-  //                                                                                                               draggable: true,
-  //                                                                                                           },
-  //                                                                                                           */
-  //
-  //     // Кнопки "вліво/вправо"
-  //     navigation: {
-  //       prevEl: ".swiper-button-prev",
-  //       nextEl: ".swiper-button-next",
-  //     },
-  //     /*
-  //                                                                                                           // Брейкпоінти
-  //                                                                                                           breakpoints: {
-  //                                                                                                               640: {
-  //                                                                                                                   slidesPerView: 1,
-  //                                                                                                                   spaceBetween: 0,
-  //                                                                                                                   autoHeight: true,
-  //                                                                                                               },
-  //                                                                                                               768: {
-  //                                                                                                                   slidesPerView: 2,
-  //                                                                                                                   spaceBetween: 20,
-  //                                                                                                               },
-  //                                                                                                               992: {
-  //                                                                                                                   slidesPerView: 3,
-  //                                                                                                                   spaceBetween: 20,
-  //                                                                                                               },
-  //                                                                                                               1268: {
-  //                                                                                                                   slidesPerView: 4,
-  //                                                                                                                   spaceBetween: 30,
-  //                                                                                                               },
-  //                                                                                                           },
-  //                                                                                                           */
-  //     // Події
-  //     on: {},
-  //   });
-  // }
+  if (document.querySelector(".product__slider")) {
+    // Вказуємо склас потрібного слайдера
+    // Створюємо слайдер
+    new Swiper(".product__slider", {
+      // Вказуємо склас потрібного слайдера
+      // Підключаємо модулі слайдера
+      // для конкретного випадку
+      modules: [Navigation, Pagination],
+      observer: true,
+      observeParents: true,
+      slidesPerView: 1,
+      spaceBetween: 20,
+      speed: 800,
+
+      //touchRatio: 0,
+      //simulateTouch: false,
+      //loop: true,
+      //preloadImages: false,
+      //lazy: true,
+
+      /*
+                                                                                                                                          // Ефекти
+                                                                                                                                          effect: 'fade',
+                                                                                                                                          autoplay: {
+                                                                                                                                              delay: 3000,
+                                                                                                                                              disableOnInteraction: false,
+                                                                                                                                          },
+                                                                                                                                          */
+
+      pagination: {
+        el: ".product__pagination",
+        clickable: true,
+      },
+
+      // Скроллбар
+      /*
+                                                                                                                                          scrollbar: {
+                                                                                                                                              el: '.swiper-scrollbar',
+                                                                                                                                              draggable: true,
+                                                                                                                                          },
+                                                                                                                                          */
+
+      // Кнопки "вліво/вправо"
+      navigation: {
+        prevEl: ".product__arrow_prev",
+        nextEl: ".product__arrow_next",
+      },
+      /*
+                                                                                                                                          // Брейкпоінти
+                                                                                                                                          breakpoints: {
+                                                                                                                                              640: {
+                                                                                                                                                  slidesPerView: 1,
+                                                                                                                                                  spaceBetween: 0,
+                                                                                                                                                  autoHeight: true,
+                                                                                                                                              },
+                                                                                                                                              768: {
+                                                                                                                                                  slidesPerView: 2,
+                                                                                                                                                  spaceBetween: 20,
+                                                                                                                                              },
+                                                                                                                                              992: {
+                                                                                                                                                  slidesPerView: 3,
+                                                                                                                                                  spaceBetween: 20,
+                                                                                                                                              },
+                                                                                                                                              1268: {
+                                                                                                                                                  slidesPerView: 4,
+                                                                                                                                                  spaceBetween: 30,
+                                                                                                                                              },
+                                                                                                                                          },
+                                                                                                                                          */
+      // Події
+      on: {},
+    });
+  }
 }
 
 // Скролл на базі слайдера (за класом swiper scroll для оболонки слайдера)
