adcd770ef1567c6f69caf9c763f92a62b3725c1d
sergii13 <sergii.khilchuk13@gmail.com>
2023-07-15 09:33:34 +0300

    complete

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -1,9 +1,15 @@
 // Підключення функціоналу "Чертоги Фрілансера"
 // Підключення списку активних модулів
 
-import { bodyLock, bodyUnlock, menuClose, menuOpen } from "./functions.js";
+import {
+  bodyLock,
+  bodyUnlock,
+  menuClose,
+  menuOpen,
+  removeClasses,
+} from "./functions.js";
 
-window.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", () => {
   const phoneBtn = document.querySelector("[data-phone]");
   if (phoneBtn) {
     phoneBtn.addEventListener("click", (e) => {
@@ -53,6 +59,56 @@ window.addEventListener("DOMContentLoaded", () => {
       document.documentElement.classList.toggle("open-catalog");
     });
   }
+  const btnCloseSearch = document.querySelector(".search-header__close");
+  if (btnCloseSearch) {
+    btnCloseSearch.addEventListener("click", (e) => {
+      e.preventDefault();
+      document.body.classList.remove("open-search");
+    });
+  }
+  const btnOpenSearch = document.querySelector(".main-header__search-open");
+  if (btnOpenSearch) {
+    btnOpenSearch.addEventListener("click", (e) => {
+      e.preventDefault();
+      document.body.classList.add("open-search");
+    });
+  }
+
+  const closeCatalog = document.querySelector(
+    ".header-catalog__close.button-icon"
+  );
+  if (closeCatalog) {
+    closeCatalog.addEventListener("click", (e) => {
+      document.documentElement.classList.remove("open-catalog");
+      bodyUnlock();
+    });
+  }
+
+  const mobNav = document.querySelector(".catalog-mob__nav");
+  if (mobNav) {
+    const allClose = document.querySelectorAll(".header-catalog__back");
+    if (allClose) {
+      allClose.forEach((item) =>
+        item.addEventListener("click", (e) => {
+          e.target.closest(".header-catalog").classList.remove("open-category");
+          removeClasses(allBlocks, "active");
+        })
+      );
+    }
+    const allBlocks = document.querySelectorAll(
+      ".header-catalog__content .header-catalog__body"
+    );
+    const allBtn = mobNav.querySelectorAll(".category-card");
+    allBtn.forEach((item, index) => {
+      item.addEventListener("click", (e) => {
+        e.target.closest(".header-catalog").classList.add("open-category");
+        window.scrollTo({ top: 0, behavior: "smooth" });
+        removeClasses(allBlocks, "active");
+        allBlocks[index].classList.add("active");
+      });
+    });
+  }
+
   document.addEventListener("click", (e) => {
     if (
       !e.target.closest(".phone-header") &&
